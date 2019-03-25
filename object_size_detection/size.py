3f8bb6cc40273a2e657792a80bf55bd0a9ebd2ff
srinivas1746 <40050770+srinivas1746@users.noreply.github.com>
2019-03-25 11:54:37 +0530

    Add files via upload

---
+++ object_size_detection/size.py
@@ -0,0 +1,95 @@
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import imutils
+
+
+path = "/home/rgukt/Desktop/criotam/friut/shape/IMG_20190325_110144.jpg"
+original = cv2.imread(path)
+
+gray = cv2.imread(path,0)
+
+gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+pixels_per_inch = None
+
+ 
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+
+
+h,w = gray.shape
+
+thresh,bin_image = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+kernel = np.ones((5,5))
+
+bin_image = bin_image-255
+
+erotion = cv2.erode(bin_image,kernel)
+
+dilation = cv2.dilate(erotion,kernel)
+
+
+
+_,cnts,_= cv2.findContours(dilation,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+
+#cnts = imutils.grab_contours(cnts)
+
+(cnts, _) = contours.sort_contours(cnts)
+
+width = 3.65   ## in inches
+
+print(len(cnts))
+
+for c in cnts:
+	if cv2.contourArea(c)<230000:
+		continue
+
+	box = cv2.minAreaRect(c)       ## will get the center,width,height,angle of rotations
+	box = cv2.boxPoints(box)     ## will get the corners
+	box = np.array(box, dtype="int")
+	box = perspective.order_points(box)
+
+
+	cv2.drawContours(original,[box.astype("int")],-1,(0,255,0),10)
+	for (x, y) in box:
+		cv2.circle(original, (int(x), int(y)), 5, (0, 0, 255), -1)         ## draw the corners
+	(topleft, topright, bottomright, bottomleft) = box                ## Top right ,top right , bottom right , bottom left
+	
+	(topleft_toprightX, topleft_toprightY) = midpoint(topleft, topright)            ## midpoint of top row of rectangele
+	(bottomleft_bottomrightX, bottomleft_bottomrightY) = midpoint(bottomleft, bottomright)              ## midpoint of bottom row of rectangle
+
+	(topleft_bottomleftX, topleft_bottomleftY) = midpoint(topleft, bottomleft)          ##  mid point of left column of rectangle
+	(topright_bottomrightX, topright_bottomrightY) = midpoint(topright, bottomright)          ##  mid point of right column of rectangle
+
+	cv2.line(original, (int(topleft_toprightX), int(topleft_toprightY)), (int(bottomleft_bottomrightX), int(bottomleft_bottomrightY)),      ## line ffrom top mid to bottom mid
+		(255, 0, 255), 2)
+	cv2.line(original, (int(topleft_bottomleftX), int(topleft_bottomleftY)), (int(topright_bottomrightX), int(topright_bottomrightY)),       ## line from left mid to right mid
+		(255, 0, 255), 2)
+	dA = dist.euclidean((topleft_toprightX, topleft_toprightY), (bottomleft_bottomrightX, bottomleft_bottomrightY))         ## height of the rectangle
+	dB = dist.euclidean((topleft_bottomleftX, topleft_bottomleftY), (topright_bottomrightX, topright_bottomrightY))         ## width of the rectangle
+
+	if pixels_per_inch is None:
+		pixels_per_inch = dA / width               ## reference size
+	print(pixels_per_inch)
+
+	dimA = dA / pixels_per_inch             ## height
+	dimB = dB / pixels_per_inch            ## width
+
+	cv2.putText(original, "{:.2f}cm".format(dimA),
+		(int(topleft_toprightX - 15), int(topleft_toprightY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
+		5, (255, 255, 255), 20)
+	cv2.putText(original, "{:.2f}cm".format(dimB),
+		(int(topright_bottomrightX -10 ), int(topright_bottomrightY-10)), cv2.FONT_HERSHEY_SIMPLEX,
+		0.5, (255, 255, 255), 2)
+
+plt.imshow(original)
+plt.show()
+
+
+
+
